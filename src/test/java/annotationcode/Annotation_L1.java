64fbe6445590c83f2666b031036884e4bb511351
Umakant P <ukp.linux@gmail.com>
2024-07-09 12:05:43 +0530

    added testNG framework structure

---
+++ src/test/java/annotationcode/Annotation_L1.java
@@ -0,0 +1,27 @@
+package annotationcode;
+
+import org.testng.annotations.Test;
+
+public class Annotation_L1 {
+	
+	
+	@Test
+	public void testAnnotation1()
+	{
+		System.out.println("first test annotation m1");
+	}
+	
+	@Test
+	public void testAnnotation2()
+	{
+		System.out.println("first test annotation m2");
+	}
+	
+	
+	@Test
+	public void testAnnotation3()
+	{
+		System.out.println("first test annotation m3");
+	}
+
+}
